8ade1a707c4a811a60fbfbbf0e30a9b704782b34
Franck Michel <fmichel@i3s.unice.fr>
2011-11-16 15:09:47 +0100

    Refactorings.

--- SE/lcg-cr-all-se.sh
+++ SE/lcg-cr-all-se.sh
@@ -5,7 +5,7 @@
 # This script is intended to be used in case Nagios is unavailable, to monitor all SEs supporting 
 # the biomed VO. The list of SEs is taken from the lcg-infosites command.
 # Results are written to files named /tmp/monitor-se/YYYYMMDD-HHMMSS.result.
-# It uses the lcg-cr.sh script version > 1.2.
+# It uses the lcg-cr.sh script version >= 1.2.
 
 VO=biomed
 TMPDIR=/tmp/monitor-se
@@ -18,7 +18,7 @@ RESULT=$TMPDIR/$NOW.result
 echo > $RESULT
 
 # Build the list of SEs from lcg-infosites: remove header lines and lines with value "n.a"
-lcg-infosites --vo $VO se | egrep -v 'Avail Space|-----' | awk '{print $4}' | sort | uniq > $TMP_LCGINFOSITES
+lcg-infosites --vo $VO space | egrep -v 'Nearline|Reserved|-----' | awk '{print $8}' | sort | uniq > $TMP_LCGINFOSITES
 
 echo "Output written to: $RESULT"
 
@@ -26,7 +26,7 @@ echo "Output written to: $RESULT"
 for SEHOSTNAME in `cat $TMP_LCGINFOSITES`
 do
   echo "----------------------------------------------------------- $SEHOSTNAME -----------" | tee -a $RESULT
-  ./lcg-cr-1.2.sh --silent $SEHOSTNAME | tee -a $RESULT
+  ./lcg-cr.sh --silent $SEHOSTNAME | tee -a $RESULT
 done
 
 echo "Removing file $TMP_LCGINFOSITES."
